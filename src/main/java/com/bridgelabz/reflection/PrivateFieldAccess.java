5a1df2a03a181ecf1cb92ae312e804c48c9db8d4
ankush1038 <ankusharma1029@gmail.com>
2025-04-06 14:24:13 +0530

    [Ankush] Add: Private field accessed

---
+++ src/main/java/com/bridgelabz/reflection/PrivateFieldAccess.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.reflection;
+
+import java.lang.reflect.Field;
+
+class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+}
+
+public class PrivateFieldAccess {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person(25);
+
+            // Get the Class object for Person
+            Class<?> cls = person.getClass();
+
+            // Get the private field 'age'
+            Field ageField = cls.getDeclaredField("age");
+
+            // Make the private field accessible
+            ageField.setAccessible(true);
+
+            // Get the current value of age
+            int currentAge = (int) ageField.get(person);
+            System.out.println("Original Age: " + currentAge);
+
+            // Modify the value of age
+            ageField.set(person, 30);
+
+            // Get the modified value of age
+            int modifiedAge = (int) ageField.get(person);
+            System.out.println("Modified Age: " + modifiedAge);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
