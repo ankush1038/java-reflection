80dc71c9edda74bc653fec6704f68bbadee76762
ankush1038 <ankusharma1029@gmail.com>
2025-04-06 16:34:42 +0530

    [Ankush] Add: Retrieve annotations at runtime

---
+++ src/main/java/com/bridgelabz/reflection/IntermediateLevel/AnnotationExample.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.reflection.IntermediateLevel;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Annotation;
+
+// Create a custom annotation
+@Retention(RetentionPolicy.RUNTIME) // Make it available at runtime
+@interface Author {
+    String name();
+}
+
+// Apply annotation to a class
+@Author(name = "John Doe")
+class Book {
+    private String title = "Reflection in Java";
+}
+
+public class AnnotationExample {
+    public static void main(String[] args) {
+        try {
+            // Get the Class object
+            Class<?> cls = Class.forName("Book");
+
+            // Check if @Author is present
+            if (cls.isAnnotationPresent(Author.class)) {
+                // Retrieve the annotation
+                Author author = cls.getAnnotation(Author.class);
+                System.out.println("Author: " + author.name());
+            } else {
+                System.out.println("@Author annotation not present.");
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
