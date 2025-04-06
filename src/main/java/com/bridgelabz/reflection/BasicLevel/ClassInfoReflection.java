192456b2c9e62ad8b3891e59b300897475c39758
ankush1038 <ankusharma1029@gmail.com>
2025-04-06 14:26:13 +0530

    [Ankush] Add: Class Reflection

--- src/main/java/com/bridgelabz/reflection/BasicLevel/ClassInfoReflection.java
+++ src/main/java/com/bridgelabz/reflection/BasicLevel/ClassInfoReflection.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ClassInfoReflection {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the fully qualified class name (e.g., java.util.ArrayList): ");
         String className = sc.nextLine();
